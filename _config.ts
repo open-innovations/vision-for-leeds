35e57dd6f1863982154480d89adc7ecf25d2cf38
Taz-Chiles <97239140+Taz-Chiles@users.noreply.github.com>
2024-07-10 11:09:02 +0100

    Install OI Lume Viz

--- _config.ts
+++ _config.ts
@@ -5,19 +5,42 @@ import date from "lume/plugins/date.ts";
 import metas from "lume/plugins/metas.ts";
 import postcss from "lume/plugins/postcss.ts";
 
+// Importing the OI Lume charts and utilities
+import oiViz from "https://deno.land/x/oi_lume_viz@v0.15.1/mod.ts";
+import autoDependency from "https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts";
+import csvLoader from "https://deno.land/x/oi_lume_utils@v0.4.0/loaders/csv-loader.ts";
+import jsonLoader from "lume/core/loaders/json.ts";
+
 const site = lume({
   src: './src',
-  // TODO Update this with the proper URL
-  location: new URL("https://open-innovations.github.io/oi-lume-template/"),
+  location: new URL("https://open-innovations.github.io/vision-for-leeds/"),
 });
+
 site.use(nunjucks()); // For Lume 2
+
+site.process([".html"], (pages) => pages.forEach(autoDependency));
+
+site.loadData([".csv", ".tsv", ".dat"], csvLoader({ basic: true }));
+site.loadData([".geojson"], jsonLoader);
+site.loadData([".hexjson"], jsonLoader);
+
+
+// Import lume viz
+import oiVizConfig from "./oi-viz-config.ts";
+site.use(oiViz(oiVizConfig));
+
 site.use(base_path());
 site.use(metas({
   defaultPageData: {
-    title: 'title', // Use the `date` value as fallback.
+    title: 'LCC Vision for Leeds Dashboard', // Use the `date` value as fallback.
   },
 }));
 site.use(date());
 site.use(postcss({}));
+
+site.copy('CNAME');
+site.copy('.nojekyll');
+site.copy('assets/images');
+site.copy('assets/css/fonts');
 
 export default site;
